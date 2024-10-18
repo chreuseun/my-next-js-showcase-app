b449abe77913934a8bc805c101ac8ca5eb37d402
chreuseun <eunillevillegaseunille@gmail.com>
2024-10-19 01:06:09 +0800

    add login button

---
+++ components/common/CustomButton.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+const CustomButton: React.FC<{
+  placeholder?: string;
+  className?: string;
+  label?: string;
+  children?: ReactNode;
+}> = ({ className = "", label = "", children }) => {
+  return (
+    <button
+      className={`
+        bg-brand2 border 
+        hover:bg-success1 
+        hover:text-text1
+        text-bg2
+        font-bold
+        py-2 px-4 
+        rounded-lg ${className}`}
+    >
+      {children ? children : label}
+    </button>
+  );
+};
+
+export default CustomButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-"use client";
-
 import TextInput from "@root/components/common/TextInput";
+import CustomButton from "@root/components/common/CustomButton";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.Js Showcase | Login page",
+  description:
+    "Welcome to Login page, enter your username and password or sign-up",
+};
 
 const LandingPage = () => {
   return (
     <div className="bg-bg2 min-h-screen flex flex-col justify-center items-center ">
       <div className="bg-bg1 p-8 rounded-2xl flex flex-col justify-center items-stretch w-auto lg:w-1/4">
-        <div className="text-center mb-2 text-brand1 text-base lg:text-4xl">
+        <div className="text-center mb-2 text-brand1 text-base lg:text-4xl font-bold">
           Next.JS
           <br />
           Showcase App
@@ -21,6 +27,7 @@ const LandingPage = () => {
           type="password"
           className="mt-3 text-center"
         />
+        <CustomButton className="mt-8" label="Login" />
       </div>
     </div>
   );
